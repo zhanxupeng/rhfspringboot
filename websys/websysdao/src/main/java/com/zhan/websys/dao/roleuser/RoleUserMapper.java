b58a1094950c8c615c0ebf08b230b39f9b236bfd
zhanxupeng <1098481123@qq.com>
2018-11-20 20:30:06 +0800

    菜单相关代码

--- websys/websysdao/src/main/java/com/zhan/websys/dao/roleuser/RoleUserMapper.java
+++ websys/websysdao/src/main/java/com/zhan/websys/dao/roleuser/RoleUserMapper.java
@@ -2,6 +2,8 @@ package com.zhan.websys.dao.roleuser;
 
 import com.zhan.websys.dao.base.BaseMapper;
 import com.zhan.websys.entity.roleuser.RoleUser;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RoleUserMapper extends BaseMapper<RoleUser> {
 }
--- websys/websysdao/src/main/java/com/zhan/websys/dao/userright/UserRightMapper.java
+++ websys/websysdao/src/main/java/com/zhan/websys/dao/userright/UserRightMapper.java
@@ -2,6 +2,8 @@ package com.zhan.websys.dao.userright;
 
 import com.zhan.websys.dao.base.BaseMapper;
 import com.zhan.websys.entity.userright.UserRight;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserRightMapper extends BaseMapper<UserRight>{
 }
